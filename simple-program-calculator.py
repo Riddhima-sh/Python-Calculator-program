c12bffe581e8f155d0f9d8805e4fc324d30b80b7
Riddhima-sh <136448829+Riddhima-sh@users.noreply.github.com>
2023-08-21 21:44:39 +0530

    simple-program-calculator.py

---
+++ simple-program-calculator.py
@@ -0,0 +1,11 @@
+x=float(input("Enter 1st number here: "))
+y=float(input("Enter 2nd number here: "))
+z=input("Enter the type of operation you want to do: ")
+if(z=="+"): 
+ print(x+y)
+if(z=="-"):
+ print(x-y)
+if(z=="*"):
+ print(x*y)
+if(z=="/"):
+ print(x/y)
